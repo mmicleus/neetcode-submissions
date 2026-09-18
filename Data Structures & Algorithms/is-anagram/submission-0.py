00a0b5ee537182d69ba6be0f8c57f70294bb3ce5
Marik Micleusanu <36306873+mmicleus@users.noreply.github.com>
2026-09-18 10:43:21 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+
+        letter_counter = {}
+
+        for ch in s:
+            if ch in letter_counter:
+                letter_counter[ch] += 1
+            else:
+                letter_counter[ch] = 1
+
+        for ch in t:
+            if ch not in letter_counter:
+                return False
+            else: 
+                letter_counter[ch] -= 1
+
+
+        for count in letter_counter.values():
+            if count != 0:
+                return False
+
+        return True
+
+
+        
